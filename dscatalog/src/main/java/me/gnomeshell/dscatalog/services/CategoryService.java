b66e47b39db2eaa62260a002b9e192f85c839687
Paulo Ricardo <pauloaraujo1211@outlook.com>
2022-07-10 19:32:47 -0300

    find category by id and exception handling

--- dscatalog/src/main/java/me/gnomeshell/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/me/gnomeshell/dscatalog/services/CategoryService.java
@@ -1,11 +1,16 @@
 package me.gnomeshell.dscatalog.services;
 
+import me.gnomeshell.dscatalog.dto.CategoryDTO;
 import me.gnomeshell.dscatalog.entities.Category;
 import me.gnomeshell.dscatalog.repositories.CategoryRepository;
+import me.gnomeshell.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -13,8 +18,21 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public List<Category> findAll(){
-        return categoryRepository.findAll();
+    // garante a integridade da transação e não deixa lockar o banco e melhorar a performance
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll(){
+        List<Category> list = categoryRepository.findAll();
+
+        return list.stream()
+                .map(x -> new CategoryDTO(x))
+                .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
 }
---
+++ dscatalog/src/main/java/me/gnomeshell/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package me.gnomeshell.dscatalog.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(String msg){
+        super(msg);
+    }
+}
